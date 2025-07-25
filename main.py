b534a82ae2f786fc80e310e2ff3b69c8c558354c
Charl_Cilliers <charl123c@gmail.com>
2025-07-25 20:47:16 +0200

    CH2 L4: count words

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+# get string data from text
 def get_book_text(book):
     with open(book) as f:
         file_contents = f.read()
     return file_contents
 
+# take text as string
+#split words into separate strings
+#count number of words/strings in text
+def count_words(book):
+    #call get_book_text to get text
+    #get_book_text(book) #(returns text as string (file_contents))
+
+    list_words = get_book_text(book).split() #split the text returned from "get_book_text()"
+    #text.split() allready makes a list of words (no need to append)
+    
+    number_of_words = len(list_words) #get number of words via len()
+    return number_of_words
+
+#main...nuff said
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
-    print(frankenstein)
+    print(f"{count_words("books/frankenstein.txt")} words found in the document")
 
 main()
